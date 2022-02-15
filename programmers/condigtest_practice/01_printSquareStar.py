7a16e34589ea48490e82fc01c3e5fafe1cd62f74
sudole <44888567+sudole@users.noreply.github.com>
2022-02-15 12:58:03 +0900

    Update 01_printSquareStar.py
    check entered value

--- programmers/condigtest_practice/01_printSquareStar.py
+++ programmers/condigtest_practice/01_printSquareStar.py
@@ -1,4 +1,8 @@
 def printSquareStar(a, b):
+    if a > 1000 or b > 1000:
+        print("The entered value must not exceed 1000.")
+        return
+    
     for i in range(b):
         s = ''
         for j in range(a):
