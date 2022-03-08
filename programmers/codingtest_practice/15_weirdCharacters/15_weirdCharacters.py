5858c01d2a1c2e92502a7209a67a4f82554dfee7
sudole <44888567+sudole@users.noreply.github.com>
2022-03-08 23:35:10 +0900

    Update 15_weirdCharacters.py

--- programmers/codingtest_practice/15_weirdCharacters/15_weirdCharacters.py
+++ programmers/codingtest_practice/15_weirdCharacters/15_weirdCharacters.py
@@ -1,13 +1,13 @@
 def solution(s):
-    if s=="": return s
-    arr = str(s).split()
-    for word in arr:
-        rp=""
-        for i, char in enumerate(list(word)):
-            rp += char.upper() if i % 2 == 0 else char.lower()
-            
-        s = s.replace(word,rp)
-    
-    return s
+    answer = ''
+    i = 0
+    for char in list(s):
+        if char == " ": 
+            i = 0
+            answer += char
+        else:
+            answer += char.upper() if i % 2 == 0 else char.lower()
+            i+=1
+    return answer
   
 print(solution("try hello world"))
