e23455f048c3b6c1b78e56d34f2bc4b2cf07ea9e
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-03-13 19:54:02 +0900

    clear...bb

--- programmers/codingtest_practice/19_ceaser/ceaser.py
+++ programmers/codingtest_practice/19_ceaser/ceaser.py
@@ -1,11 +1,19 @@
 def solution(s, n):
     answer = ''
     for c in list(s):
-        ordnum = ord(c)
-        ceaser = ordnum + n
-        
-        if ceaser > 122 or (ceaser > 90 and ceaser < 97): ceaser -= 26
-        if ceaser < 65 or ceaser > 122 or (ceaser > 90 and ceaser < 97): answer += c
+        if c == " ": answer += c
         else:
-            answer += chr(ceaser)
+            end=90
+            ordnum=ord(c)
+            if ordnum >= 97: end=122
+            ceaser = (ordnum+n)%end
+            
+            if ceaser == 0: ceaser = end
+            elif ceaser < (end-26): ceaser += (end-26)
+            answer+=chr(ceaser)
+    
     return answer
+
+print(solution('AB',1))
+print(solution('z',1))
+print(solution('a B z',4))
