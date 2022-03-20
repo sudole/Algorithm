f508950f43a42c5156f4374552053dda7a3d2a59
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-03-20 23:25:28 +0900

    내맘대로정렬하기 (진행중)

--- programmers/codingtest_practice/25_mywaySort/mywaySort.py
+++ programmers/codingtest_practice/25_mywaySort/mywaySort.py
@@ -1,4 +1,16 @@
 def solution(s, n):
-    return []
+    answer=[]
+    w=[]
+    for i, word in enumerate(s):
+        w.append((i, word[n]))
+    
+    # print(w)
+    c_w=sorted(w, key = lambda item: item[1])
+    # print(c_w)
+
+    for x in c_w:
+        answer.append(s[x[0]])
+    return answer
 
 print(solution(["sun", "bed", "car"], 1))
+print(solution(["abce", "abcd", "cdx"], 2))
