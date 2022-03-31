86984c412c8ae9675e4f890646945557413da857
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-03-31 21:31:15 +0900

    다트게임 진행중...

---
+++ programmers/codingtest_practice/32_dartGame/dartGame.py
@@ -0,0 +1,30 @@
+def solution(dartResult):
+    answer = 0
+
+    while len(dartResult)>0:
+        idx = findChar(dartResult)
+
+        chkstr=''
+        if idx != 0:
+            chkstr = dartResult[0:idx]
+        else:
+            chkstr = dartResult[idx]
+        
+        dartResult = dartResult.replace(chkstr, '')
+        print(idx, chkstr, dartResult)
+    # for x in dartResult:
+    #     print(x)
+    return answer
+
+def findChar(strWord):
+    index = -1
+    for x in ['S', 'D', 'T']:
+        index = strWord.find(x)
+        if index > -1:
+            break
+    return index
+
+print(solution('1S2D*3T'))
+print(ord('0'))
+print(ord('1'))
+print(ord('9'))
