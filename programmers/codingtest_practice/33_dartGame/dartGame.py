8239947aba8f213d17f0e3bf3f7d537badb9e9d3
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-04-02 23:54:22 +0900

    다트게임 진행중-2

--- programmers/codingtest_practice/33_dartGame/dartGame.py
+++ programmers/codingtest_practice/33_dartGame/dartGame.py
@@ -1,23 +1,28 @@
 def solution(dartResult):
     answer = 0
+    score_item = []
 
     while len(dartResult)>0:
-        idx = findChar(dartResult)
-
         chkstr=''
-        if idx != 0:
-            chkstr = dartResult[0:idx]
-        else:
-            chkstr = dartResult[idx]
-        
+        idx = findBonus(dartResult)
+        o_idx = findOption(dartResult)
+
+        if o_idx-idx == 1:
+            chkstr = dartResult[0:o_idx+1]
+        elif idx > -1:
+            chkstr = dartResult[0:idx+1]
+            
+        score_item.append(chkstr)
         dartResult = dartResult.replace(chkstr, '')
-        print(idx, chkstr, dartResult)
-        
-    # for x in dartResult:
-    #     print(x)
+
+    for score in score_item:
+        b_idx=findBonus(score)
+        n = score[0:b_idx]
+        b = score.replace(n, '')
+        print(score, b_idx, n, b)
     return answer
 
-def findChar(strWord):
+def findBonus(strWord):
     index = -1
     for x in ['S', 'D', 'T']:
         index = strWord.find(x)
@@ -25,7 +30,12 @@ def findChar(strWord):
             break
     return index
 
+def findOption(strWord):
+    index = -1
+    for x in ['*', '#']:
+        index = strWord.find(x)
+        if index > -1:
+            break
+    return index
+
 print(solution('1S2D*3T'))
-print(ord('0'))
-print(ord('1'))
-print(ord('9'))
