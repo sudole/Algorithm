b02504cff09ec9b9241437e2036d2b62941db4cb
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-04-07 23:55:20 +0900

    다트게임

--- programmers/codingtest_practice/33_dartGame/dartGame.py
+++ programmers/codingtest_practice/33_dartGame/dartGame.py
@@ -1,65 +1,35 @@
 def solution(dartResult):
-    answer = 0
-    score_item = []
-    
-    while len(dartResult)>0:
-        chkstr=''
-        idx = findBonus(dartResult)
-        o_idx = findOption(dartResult)
-
-        if o_idx-idx == 1:
-            chkstr = dartResult[0:o_idx+1]
-        elif idx > -1:
-            chkstr = dartResult[0:idx+1]
-            
-        score_item.append(chkstr)
-        dartResult = dartResult.replace(chkstr, '')
-
-    for score in score_item:
-        b_idx=findBonus(score)
-        n = score[0:b_idx]
-        b = score.replace(n, '')
-        o_idx = findOption(b)
-
-        n = int(n)
-        o = convertOption(b[o_idx])
-        if o_idx > -1 and o > -1:
-            b = convertBonus(b[0:o_idx])
-            answer = answer * o + (n ** b) * o
+    x_num = -1
+    tmp_answer = []
+    for x in dartResult:
+        x_ord=ord(x)
+        if x_ord >= 48 and x_ord <= 57:
+            if x_ord == 48 and x_num > -1:
+                x_num *= 10
+            else:
+                x_num = int(x)
         else:
-            answer = answer + (n ** convertBonus(b))
+            if x in ('S', 'D', 'T'):
+                if x == 'S':
+                    tmp_answer.append(x_num**1)
+                elif x == 'D':
+                    tmp_answer.append(x_num**2)
+                elif x == 'T':
+                    tmp_answer.append(x_num**3)
+            elif x in ('#', '*'):
+                last_idx = len(tmp_answer)-1
+                if x == '#':
+                    tmp_answer[last_idx] = tmp_answer[last_idx]*-1
+                elif x == '*':
+                    tmp_answer = tmp_answer[:last_idx-1] + [a*2 for a in tmp_answer[last_idx-1:]]
+            x_num = -1
 
-    return answer
-
-def findBonus(strWord):
-    index = -1
-    for x in ['S', 'D', 'T']:
-        index = strWord.find(x)
-        if index > -1:
-            break
-    return index
-
-def findOption(strWord):
-    index = -1
-    for x in ['*', '#']:
-        index = strWord.find(x)
-        if index > -1:
-            break
-    return index
-
-def convertBonus(b):
-    if b == 'S':
-        return 1
-    if b == 'D':
-        return 2
-    if b == 'T':
-        return 3
-
-def convertOption(o):
-    if o == '*':
-        return 2
-    if o == '#':
-        return -1
+    return sum(tmp_answer)
 
 print(solution('1S2D*3T'))
 print(solution('1D2S#10S'))
+print(solution('1D2S0T'))
+print(solution('1S*2T*3S'))
+print(solution('1D#2S*3S'))
+print(solution('1T2D3D#'))
+print(solution('1D2S3T*'))
