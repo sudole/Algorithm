4ec4cd2f699f1459d00d0376638c330a2dc1fca5
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-04-10 02:36:08 +0900

    보물지도

--- programmers/codingtest_practice/34_treasureMap/treasureMap.py
+++ programmers/codingtest_practice/34_treasureMap/treasureMap.py
@@ -1,16 +1,14 @@
 def solution(n, arr1, arr2):
     answer = []
     for x in range(n):
-        b1 = bin(arr1[x])
-        b2 = bin(arr2[x])
-        print(arr1[x], b1, len(b1))
-        print(arr2[x], b2)
-        print(str(b1 and b2), b1 and b2)
-        
-        #answer[x] = b1 or b2
+        b = bin(arr1[x] | arr2[x])
+        b = b[2:].zfill(n)
+        answer.append(b.replace('1', '#').replace('0', ' '))
     return answer
 
 
 print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
+print(solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10]))
 
-#print(format(9, 'b'))
+# print(format(9, '010b'))
+# print(format(30, '010b'))
