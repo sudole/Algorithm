c2a6a527f5173e648ca5541458e77ff3e088d311
sudole <44888567+sudole@users.noreply.github.com>
2022-11-27 14:47:12 +0900

    Update makeHamburger.py

--- programmers/codingtest_practice/40_makeHamburger/makeHamburger.py
+++ programmers/codingtest_practice/40_makeHamburger/makeHamburger.py
@@ -1,17 +1,13 @@
 def solution(ingredient):
     answer = 0
-    arr_ingre = [1, 2, 3, 1]
-    chk_ingre = 0
-    for ing in ingredient:
-        if chk_ingre == 3:
-            answer+=1
-            chk_ingre = 0
-        if arr_ingre[chk_ingre] == ing:
-            chk_ingre+=1
-        elif arr_ingre[chk_ingre-1] == ing:
-            pass
-        else:
-            chk_ingre = 0
+    str_match = '1231'
+    str_ingre = ''.join([str(x) for x in ingredient])
+    
+    idx = str_ingre.find(str_match)
+    while(idx > -1):
+        idx = str_ingre.find(str_match)
+        answer=answer+1
+        str_ingre = str_ingre[0:idx-1] + str_ingre[idx+5:len(str_ingre)]
     return answer
   
 print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
