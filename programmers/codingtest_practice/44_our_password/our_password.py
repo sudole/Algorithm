11c058ed932075fab72ad6ecc7de42538c9a9cc4
sjlim <resqpa79@gmail.com>
2023-02-26 17:55:25 +0900

    둘만의 암호

--- programmers/codingtest_practice/44_our_password/our_password.py
+++ programmers/codingtest_practice/44_our_password/our_password.py
@@ -1,18 +1,21 @@
-def calc(x, index, skips):
-    calculating = True
-    calc_num = int((ord(x)+index-97) % 26) + 97
-    while calculating:
-        for n in skips:
-            if ord(x) < calc_num and n >= ord(x) and n <= calc_num:
-                calc_num += 1
-            elif calc_num < ord(x) and n <= ord(x) and n >= calc_num:
-                calc_num += 1
-            else:
-                calculating = False
-        calc_num = int((calc_num-97) % 26) + 97
-    return chr(calc_num)
+def update_word(word, i):
+    return chr((ord(word) + i - ord('a')) % 26 + ord('a'))
+
+def calc(word, index, skip):
+    u1 = word
+    for i in range(1, index+1):
+        u1 = update_word(u1, 1)
+        while u1 in skip:
+            u1 = update_word(u1, 1)
+    return u1
+
 
 def solution(s, skip, index):
-    arr_skip = [ord(x) for x in skip]
-    answer = ''.join([calc(x, index, arr_skip) for x in s])
+    answer = ''
+
+    for w in s:
+        answer += calc(w, index, skip)
+
     return answer
+# answer "happy"
+print(solution("aukks", "wbqd", 5))
