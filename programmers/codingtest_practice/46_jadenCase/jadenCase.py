448d3301971acaeb753e78da4890d73a63d1b0c3
sjlim <resqpa79@gmail.com>
2023-03-07 22:01:02 +0900

    JadenCase #4

--- programmers/codingtest_practice/46_jadenCase/jadenCase.py
+++ programmers/codingtest_practice/46_jadenCase/jadenCase.py
@@ -2,9 +2,13 @@ def solution(s):
     answer = ''
 
     while s.find(' ') != -1:
-        answer += s[0].upper()
-        answer += s[1:s.find(' ')+1].lower()
-        s=s.replace(s[:s.find(' ')+1], '')
+        if s.find(' ') == 0:
+            answer += s[0]
+            s = s[1:]
+        else:
+            answer += s[0].upper()
+            answer += s[1:s.find(' ')+1].lower()
+            s=s.replace(s[:s.find(' ')+1], '')
     answer += s[0].upper() + s[1:].lower()
 
     return answer
