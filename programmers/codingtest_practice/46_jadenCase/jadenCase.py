a8599f02aaa1fbf3e083655b156ebd1f697a5f89
sjlim <resqpa79@gmail.com>
2023-03-12 22:59:10 +0900

    JadenCase final

--- programmers/codingtest_practice/46_jadenCase/jadenCase.py
+++ programmers/codingtest_practice/46_jadenCase/jadenCase.py
@@ -1,6 +1,5 @@
 def solution(s):
     answer = ''
-
     while s.find(' ') != -1:
         if s.find(' ') == 0:
             answer += s[0]
@@ -11,9 +10,18 @@ def solution(s):
         else:
             answer += s[0].upper()
             answer += s[1:s.find(' ')+1].lower()
-            s=s.replace(s[:s.find(' ')+1], '')
+            s=s[s.find(' ')+1:]
     if len(s) > 0:
         answer += s[0].upper()
     if len(s) > 1:
         answer += s[1:].lower()
     return answer
+
+# answer: "3people Unfollowed Me"
+print(solution("3people unFollowed me"))
+# answer: "For The Last Week"
+print(solution("for the last week"))
+# answer: "1a  Bcd  4ad"
+print(solution("1a  bCd  4AD"))
+# answer: "Abc     D"
+print(solution("abc     d"))
