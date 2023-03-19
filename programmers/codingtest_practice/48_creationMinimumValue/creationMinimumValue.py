25ab714c4721c1e5f5db2aa0949d89c3577b59c5
sjlim <resqpa79@gmail.com>
2023-03-19 18:00:30 +0900

    최솟값 만들기 final - hint

--- programmers/codingtest_practice/48_creationMinimumValue/creationMinimumValue.py
+++ programmers/codingtest_practice/48_creationMinimumValue/creationMinimumValue.py
@@ -1,16 +1,11 @@
 def solution(A,B):
     answer = 0
 
-    while len(A) != 0 and len(B) != 0:
-        val_a = max(A)
-        val_b = max(B)
-        if val_a < val_b:
-            val_a = min(A)
-        else:
-            val_b = min(B)
-        answer += val_a * val_b
-        A.pop(A.index(val_a))
-        B.pop(B.index(val_b))
+    A.sort()
+    B.sort(reverse=True)
+
+    for i in range(len(A)):
+        answer += A[i] * B[i]
 
     return answer
 
