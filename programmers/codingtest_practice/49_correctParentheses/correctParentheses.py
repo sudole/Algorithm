8a70683b8ce40ca85ea7586f4fe1995c81bbba1b
sjlim <resqpa79@gmail.com>
2023-03-26 20:54:28 +0900

    올바른 괄호 finish

--- programmers/codingtest_practice/49_correctParentheses/correctParentheses.py
+++ programmers/codingtest_practice/49_correctParentheses/correctParentheses.py
@@ -6,11 +6,12 @@ def solution(s):
 
     check = 0
     for x in s:
+        if check == 0 and x == ')': return False
         if x == '(':
             check+=1
         elif x == ')':
             check-=1
-    if check > 0: answer = False
+    if check != 0: answer = False
 
     return answer
 
